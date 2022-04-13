8f8354406d9edfd4d07f95aae980ae5052cb0dfb
alexiscjscab <alexiscjscab@gmail.com>
2022-04-13 20:20:30 -0300

    FIX

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import {
   makeStyles,
@@ -10,42 +10,42 @@ import {
   Divider,
   ListItem,
   ListItemIcon,
-} from '@material-ui/core';
+} from "@material-ui/core";
 
 // Iconos
-import BuildTwoToneIcon from '@material-ui/icons/BuildTwoTone';
-import InfoTwoToneIcon from '@material-ui/icons/InfoTwoTone';
-import EmojiObjectsTwoToneIcon from '@material-ui/icons/EmojiObjectsTwoTone';
-import ContactMailTwoToneIcon from '@material-ui/icons/ContactMailTwoTone';
-import MenuIcon from '@material-ui/icons/Menu';
-import CancelIcon from '@material-ui/icons/Cancel';
+import BuildTwoToneIcon from "@material-ui/icons/BuildTwoTone";
+import InfoTwoToneIcon from "@material-ui/icons/InfoTwoTone";
+import EmojiObjectsTwoToneIcon from "@material-ui/icons/EmojiObjectsTwoTone";
+import ContactMailTwoToneIcon from "@material-ui/icons/ContactMailTwoTone";
+import MenuIcon from "@material-ui/icons/Menu";
+import CancelIcon from "@material-ui/icons/Cancel";
 
 // Link react scroll
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 
 const items = [
   {
-    id: 'about',
-    text: 'About Me',
-    icon: <InfoTwoToneIcon fontSize='large' />,
+    id: "about",
+    text: "About Me",
+    icon: <InfoTwoToneIcon fontSize="large" />,
   },
 
   {
-    id: 'skills',
-    text: 'Skills',
-    icon: <EmojiObjectsTwoToneIcon fontSize='large' />,
+    id: "skills",
+    text: "Skills",
+    icon: <EmojiObjectsTwoToneIcon fontSize="large" />,
   },
 
   {
-    id: 'work',
-    text: 'My Work',
-    icon: <BuildTwoToneIcon fontSize='large' />,
+    id: "work",
+    text: "My Work",
+    icon: <BuildTwoToneIcon fontSize="large" />,
   },
 
   {
-    id: 'contact',
-    text: 'Contact',
-    icon: <ContactMailTwoToneIcon fontSize='large' />,
+    id: "contact",
+    text: "Contact",
+    icon: <ContactMailTwoToneIcon fontSize="large" />,
   },
 ];
 
@@ -56,15 +56,20 @@ const NavBar = () => {
   const [colorChange, setColorChange] = useState(false);
 
   const changeColorNavBar = () => {
-    if(window.scrollY >= 800) setColorChange(true)
-    else setColorChange(false)
-  }
+    if (window.scrollY >= 770) setColorChange(true);
+    else setColorChange(false);
+  };
 
-  window.addEventListener('scroll', changeColorNavBar);
+  window.addEventListener("scroll", changeColorNavBar);
 
-  
   return (
-    <AppBar  className={classes.root} style={{backgroundColor: colorChange ? '#000' : 'rgba(230,230,230,0.88)', color: colorChange ? '#fff' : '#000'}}>
+    <AppBar
+      className={classes.root}
+      style={{
+        backgroundColor: colorChange ? "#000" : "rgba(230,230,230,0.88)",
+        color: colorChange ? "#fff" : "#000",
+      }}
+    >
       <Toolbar className={classes.toolbar}>
         <List className={classes.menu}>
           {items.map(({ id, text }, i) => (
@@ -75,26 +80,26 @@ const NavBar = () => {
               smooth={true}
               duration={500}
               offset={-70}
-              activeClass='active'
+              activeClass="active"
             >
               {text}
             </Link>
           ))}
         </List>
         <IconButton
-          edge='end'
+          edge="end"
           className={classes.menuButton}
           onClick={() => setOpen(!open)}
         >
-          <MenuIcon fontSize='large' />
+          <MenuIcon fontSize="large" />
         </IconButton>
       </Toolbar>
       {/* Menu despleglable */}
-      <Drawer anchor='right' open={open} onClose={() => setOpen(false)}>
+      <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
         <div className={classes.drawer}>
           <div className={classes.iconButton}>
             <IconButton onClick={() => setOpen(false)}>
-              <CancelIcon fontSize='large' className={classes.cancelIcon} />
+              <CancelIcon fontSize="large" className={classes.cancelIcon} />
             </IconButton>
           </div>
           <Divider className={classes.divider} />
@@ -106,12 +111,12 @@ const NavBar = () => {
               smooth={true}
               duration={500}
               offset={-70}
-              activeClass='active'
+              activeClass="active"
               className={classes.sidebar}
             >
-              <ListItem component='h5'>
+              <ListItem component="h5">
                 <ListItemIcon
-                  style={{ color: '#fff', position: 'relative', bottom: '4px' }}
+                  style={{ color: "#fff", position: "relative", bottom: "4px" }}
                 >
                   {icon}
                 </ListItemIcon>
@@ -128,75 +133,75 @@ const NavBar = () => {
 export default NavBar;
 
 const useStyles = makeStyles((theme) => ({
-  root: {    
+  root: {
     fontFamily: "'Josefin Sans', sans-serif",
   },
   toolbar: {
-    position: 'sticky',
-    top: '0',
-    height: '60px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    position: "sticky",
+    top: "0",
+    height: "60px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   menu: {
-    [theme.breakpoints.down('sm')]: {
-      display: 'none',
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
     },
-    '& a': {
-      fontSize: '1.4rem',
-      fontWeight: 'bold',
+    "& a": {
+      fontSize: "1.4rem",
+      fontWeight: "bold",
       marginLeft: theme.spacing(3),
-      '&:hover': {
-        cursor: 'pointer',
-        color: '#2082d8',
-        borderBottom: '3px solid #2082d8',
+      "&:hover": {
+        cursor: "pointer",
+        color: "#2082d8",
+        borderBottom: "3px solid #2082d8",
       },
     },
   },
   menuButton: {
-    display: 'none',
-    [theme.breakpoints.down('sm')]: {
-      display: 'block',
-      color: '#2082d8',
-      position: 'absolute',
+    display: "none",
+    [theme.breakpoints.down("sm")]: {
+      display: "block",
+      color: "#2082d8",
+      position: "absolute",
       top: 0,
       right: 12,
     },
   },
   drawer: {
-    background: '#2082d8',
-    height: '130%',
+    background: "#2082d8",
+    height: "130%",
     fontFamily: "'Josefin Sans', sans-serif",
   },
   sidebar: {
-    [theme.breakpoints.down('sm')]: {
-      width: '40vw',
+    [theme.breakpoints.down("sm")]: {
+      width: "40vw",
     },
-    '& h5': {
+    "& h5": {
       margin: theme.spacing(8, 0, 0, 0),
-      fontSize: '1.4rem',
-      fontWeight: 'bold',
-      transition: theme.transitions.create(['all'], {
+      fontSize: "1.4rem",
+      fontWeight: "bold",
+      transition: theme.transitions.create(["all"], {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.enteringScreen,
       }),
-      '&:hover': {
-        cursor: 'pointer',
-        '& span': {
-          borderBottom: '3px solid #000',
+      "&:hover": {
+        cursor: "pointer",
+        "& span": {
+          borderBottom: "3px solid #000",
         },
       },
     },
   },
   cancelIcon: {
-    color: '#fff',
-    '&:hover': {
-      color: '#000',
+    color: "#fff",
+    "&:hover": {
+      color: "#000",
     },
   },
   iconButton: {
-    display: 'flex',
-    justifyContent: 'flex-end',
+    display: "flex",
+    justifyContent: "flex-end",
   },
 }));
